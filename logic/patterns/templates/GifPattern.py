minor fixes in patterns (GIF rotation, point width)

logic/patterns/templates/GifPattern.py
@@ -51,7 +51,7 @@ class GifPattern(PatternTemplate):
                     np.array(
                         frame.copy().convert('RGB').getdata(),
                         dtype=np.uint8
-                    ).reshape(width, height, n_channels)
+                    ).reshape(height, width, n_channels)
                 )
         result = cls(
             filename=file.name,
@@ -83,7 +83,7 @@ class GifPattern(PatternTemplate):
 
     def update_resized(self, size: Tuple[int, int]):
         width, height = size
-        self.resized_frames = np.zeros((self.n_frames, width, height, n_channels))
+        self.resized_frames = np.zeros((self.n_frames, height, width, n_channels))
         for f in range(self.n_frames):
             for ch in range(n_channels):
                 frame = self._original_frames[f, :, :, ch]
@@ -130,7 +130,7 @@ class GifPatternState(PatternInstanceState):
         index = self.frame_index % len(frames)
         frame = frames[index]
         # TODO: for now, truncate coordinates -> improve by interpolating when necessary
-        rgb = frame[int(x), int(y)]
+        rgb = frame[int(y), int(x)]
         return HsvColor.from_rgb(*rgb, scale=255)
 
     def collect_broadcast_info(self):

model/state.py
@@ -137,7 +137,12 @@ class SequenceState:
                 pixel = instance.pixels[pixel_index]
                 if pixel is None:
                     continue
-                mix = sqrt(0.01 * pixel.v) if pixel.v > 0 else 0
+                mix = 0.01 * pixel.v * pattern.opacity
+                if mix <= 0:
+                    continue
+                # this improved things:
+                mix = sqrt(mix)
+
                 rgb = pixel.to_float_rgb()
                 for b in range(self.bytesize):
                     try:
@@ -150,7 +155,7 @@ class SequenceState:
 
                 if self.verbose:
                     print(f"-- Fill into RGB array: Pixel {pixel_index} => index {index}: color={rgb}, " +
-                                  f"mix {mix}, result={self._rgb_array[index:index + self.bytesize]}")
+                          f"mix {mix}, result={self._rgb_array[index:index + self.bytesize]}")
 
         self._rgb_array = np.clip(
             np.multiply(self._rgb_array, 255),